db6f224974bb820ec3dee358afa8ed988e5219dd
Promila1 <115773798+Promila1@users.noreply.github.com>
2022-10-26 11:47:24 +0530

    Add files via upload

---
+++ p2.java
@@ -0,0 +1,15 @@
+package first;
+
+public class p2 {
+	public static void  main(String args[]) {
+	int n=4;
+	for(int i=0;i<n;i++) {
+		for(int j=0;j<=n-1;j++) {
+			
+			System.out.print(i+1);
+			}
+		System.out.println();
+	}
+}
+}
+
